c673718c406796696b58b16f0ec052d222ed05ec
Sam Bremer <Sam.Bremer@hpsnz.org.nz>
2023-04-13 16:25:00 +1200

    Regular auto-commit at 16:25:00 on 13/04/2023

--- pages/TP_Tool.py
+++ pages/TP_Tool.py
@@ -21,24 +21,91 @@ import datetime
 st.set_page_config(page_title='CNZ Performance Database',
                   page_icon=":bike:",
                   layout="wide")
-st.header('Pursuit Visualiser')
 
-def get_data_from_excel():
-    df_master = pd.read_excel(
-        io='pages/TP_Master.xlsx',
-        engine ='openpyxl',
-        sheet_name='Sheet1',
-        skiprows=0,
-        usecols='A:I',
-        nrows=8000
-        )
-    #df = df.replace(',','', regex=True)
-    #for i in range(len(df)):
-        #df["Date"][i] = df["Date"][i].date()
-        #if df["125m"][i] != "NULL":
-            #df["125m"][i] = df["125m"][i].strftime("%M:%S.%f")
-    return df_master
-df_master= get_data_from_excel()
+
+# def get_data_from_excel():
+#     df_master = pd.read_excel(
+#         io='pages/TP_Master.xlsx',
+#         engine ='openpyxl',
+#         sheet_name='Sheet1',
+#         skiprows=0,
+#         usecols='A:I',
+#         nrows=8000
+#         )
+#     #df = df.replace(',','', regex=True)
+#     #for i in range(len(df)):
+#         #df["Date"][i] = df["Date"][i].date()
+#         #if df["125m"][i] != "NULL":
+#             #df["125m"][i] = df["125m"][i].strftime("%M:%S.%f")
+#     return df_master
+# df_master= get_data_from_excel()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##This bit is the historical visualiser
+
+st.header("View/compare efforts from master file")
+master_file=st.file_uploader("Select master file")
+if master_file:
+    df_master = pd.read_excel(master_file)
+    df_master
+
+    selections = st.multiselect(
+    "Select past effort(s):",
+    options=df_master["Title"].unique()
+    )   
+
+
+
+
+    if len(selections) !=0:
+        for i in range(len(selections)):
+            col_1,col_2=st.columns(2)
+            with col_1:
+                df_temp = df_master.loc[df_master['Title'] == selections[i]]
+                df_temp
+            with col_2:
+                fig = px.bar(df_temp, x='Distance', y='Avg_Speed',color=df_temp.Front,hover_data=[df_temp.Split, df_temp.Avg_Speed,df_temp.Del_Speed])
+                fig.add_trace(go.Scatter(x=df_temp['Distance'][1:], y=df_temp['Del_Speed'][1:],mode='markers',name="Delivery Speed"))
+                fig.update_layout(
+                title={
+                    'text': df_temp.Title.iloc[0],
+                    'y':0.9,
+                    'x':0.5,
+                    'xanchor': 'center',
+                    'yanchor': 'top',
+                    'font':dict(size=25)})
+                #fig.add_hline(y=250*3.6/schedule, line_dash="dash",line_color="white",annotation_text="Schedule = " +str(schedule))
+                st.plotly_chart(fig, use_container_width=True)
+            
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+st.markdown("---")            
+st.header('View, edit and upload a new effort')
 
 with open('pages/TP_demo.xlsx', "rb") as template_file:
         template_byte = template_file.read()
@@ -50,41 +117,7 @@ st.download_button(label="Click to Download Template File",
                     file_name="template.xlsx"
                   )
 
-riders=["GATE Aaron","BRIDGEWATER Dan","SEXTON Tom", "GOUGH Regan", "STEWART Campbell", "JACKSON George"]
-riders.sort()
-
-uploaded_file = st.file_uploader("Choose a file")
-##This bit is the historical visualiser
-selections = st.multiselect(
-"Or select from past effort(s):",
-options=df_master["Title"].unique()
-)   
-
-
-
-
-if len(selections) !=0:
-    for i in range(len(selections)):
-        col_1,col_2=st.columns(2)
-        with col_1:
-            df_temp = df_master.loc[df_master['Title'] == selections[i]]
-            df_temp
-        with col_2:
-            fig = px.bar(df_temp, x='Distance', y='Avg_Speed',color=df_temp.Front,hover_data=[df_temp.Split, df_temp.Avg_Speed,df_temp.Del_Speed])
-            fig.add_trace(go.Scatter(x=df_temp['Distance'][1:], y=df_temp['Del_Speed'][1:],mode='markers',name="Delivery Speed"))
-            fig.update_layout(
-            title={
-                'text': df_temp.Title.iloc[0],
-                'y':0.9,
-                'x':0.5,
-                'xanchor': 'center',
-                'yanchor': 'top',
-                'font':dict(size=25)})
-            #fig.add_hline(y=250*3.6/schedule, line_dash="dash",line_color="white",annotation_text="Schedule = " +str(schedule))
-            st.plotly_chart(fig, use_container_width=True)
-            
-
-
+uploaded_file = st.file_uploader("Choose a file",key="uploader")
 
 if uploaded_file is not None:
     st.markdown("---")
@@ -152,19 +185,23 @@ if uploaded_file is not None:
     
     
     
-    path_to_master=st.text_input("Path to your master document:")
     
     
+    master_path=st.text_input("Add path to master file:",key="prompt")
     if st.button("Save this effort to master",key="upload"):
+        
+        
+  
+      
         df_save=df
         df_save.insert(0, 'Save_Date', datetime.date.today())
         df_save.insert(0, 'Title', Title)
         df_save
         st.write("Saved to Database")
 
- 
-        
-        master=xw.Book(f'{path_to_master}')
+
+
+        master=xw.Book(f'{master_path}')
         master_sheets=master.sheets
         sheet=master_sheets[0]
         if sheet.range('A1').end('down').row > 1000000:
@@ -184,8 +221,7 @@ if uploaded_file is not None:
             index+=1
         master.save()
         master.close()
-        
-            
-            
-            
-  
+
+
+
+
